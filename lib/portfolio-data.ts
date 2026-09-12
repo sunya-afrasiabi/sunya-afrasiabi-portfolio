cc2e6b1293e4591f731308ee6cda784c45b7122c
Sunya Afrasiabi <sunyaafrasiabi@gmail.com>
2026-09-12 17:18:01 -0400

    Remove local image from portfolio gallery
    Removed a local image reference from the gallery.

--- lib/portfolio-data.ts
+++ lib/portfolio-data.ts
@@ -89,7 +89,6 @@ export const projects: Project[] = [
     image: "/projects/compos.jpg", 
     gallery: [
       "https://youtu.be/Tlzn4IAGWmQ", // Slide 1: Your YouTube video
-      "/projects/chassis.png.jpg",   // Slide 2: A local image you already have
       "/sunya-profile.jpg"           // Slide 3: Another local image
     ],
     hideHeroImage: true,
